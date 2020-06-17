38c860584eca9a7bc39022b3dc254944a831f222
Chyna Davis <chyna.davis11@gmail.com>
2020-06-17 11:36:30 -0700

    add build

---
+++ build/precache-manifest.f8ae444f71d5827ea8e2ea49de5a40ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8080452b7c01a10765331b5a5da8dd",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "41cf5756283df7ea5f02",
+    "url": "/ExpenseTracker/static/css/main.81946054.chunk.css"
+  },
+  {
+    "revision": "1aa3ed1321ee30ff57a5",
+    "url": "/ExpenseTracker/static/js/2.e596713e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTracker/static/js/2.e596713e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cf5756283df7ea5f02",
+    "url": "/ExpenseTracker/static/js/main.a359d827.chunk.js"
+  },
+  {
+    "revision": "6a4761a450062a671a7d",
+    "url": "/ExpenseTracker/static/js/runtime-main.de1c3c37.js"
+  }
+]);
